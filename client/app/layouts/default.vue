4e9090fd433446b85c1b0f0bc41920cab1fa4bb2
watcharapon weeraborirak <69833839+watcharap0n@users.noreply.github.com>
2022-08-04 15:36:40 +0700

    default.vue

--- client/app/layouts/default.vue
+++ client/app/layouts/default.vue
@@ -6,8 +6,14 @@
         fixed
         :style="`margin-left: ${$vuetify.application.left}px;`"
         color="black"
+        dark
     >
       <v-toolbar-title style="color: #D0DA52">EXKASAN</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <div>หน้าหลัก</div>/&nbsp;
+      <div>การให้บริการ</div>/&nbsp;
+      <div>ระบบตรวจสอบ</div>
 
     </v-app-bar>
 
@@ -16,9 +22,15 @@
     >
 
       <Nuxt/>
-
     </div>
 
+
+    <footer class="text-center">
+      <small>This site is protected by reCAPTCHA and the Google
+        <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+        <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+      </small>
+    </footer>
   </v-app>
 </template>
 
