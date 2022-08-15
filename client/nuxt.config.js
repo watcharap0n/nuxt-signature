65be83353e3d2b425d3283a9704246b15a03df47
apichartw <apichartw@thaicom.net>
2022-08-15 17:09:14 +0700

    edit nuxt config remove http-equiv

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -20,8 +20,7 @@ module.exports = {
         meta: [
             {charset: 'utf-8'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: 'Meta description'},
-            {'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests'}
+            {hid: 'description', name: 'description', content: 'Meta description'}
         ],
 
         script: [
