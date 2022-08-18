a64538cc562bca2e42f95db7461d20b0ab64c1c5
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2022-08-18 16:15:48 +0700

    update exkasan.vue

--- client/app/pages/exkasan.vue
+++ client/app/pages/exkasan.vue
@@ -1,127 +1,66 @@
 <template>
   <v-container>
-
     <v-card
         flat
         style="margin-top: 40px"
         v-if="transaction"
     >
-      <v-list class="bg-light">
-        <v-list-group
-            :value="true"
-            prepend-icon="mdi-file-account"
-        >
-          <template v-slot:activator>
-            <v-list-item-title>ข้อมูลลายเซนต์อิเล็กทรอนิกส์ (Digital Signature)</v-list-item-title>
-          </template>
-
+      <div v-for="(v, k) in transaction.signatures" :key="k">
+        <br><br>
+        <v-list>
           <v-list-group
-              v-for="(v, k) in transaction.signatures"
+              :value="true"
               v-if="v.dsSignerCertificateDn"
               :key="k"
-              :value="true"
-              no-action
-              sub-group
               prepend-icon="mdi-draw-pen"
+              no-action
           >
             <template v-slot:activator>
               <v-list-item-content>
-                <v-list-item-title>Sign</v-list-item-title>
+                <v-list-item-title>Rev.{{ k + 1 }} ข้อมูลลายมือชื่อดิจิทัล (Digital Signature)</v-list-item-title>
               </v-list-item-content>
             </template>
 
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>ผู้ลงลายมือชื่อดิจิตอล</v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-content>
-                {{ v.dsSignerCertificateDn.issuerDn.commonName }}
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>วันออกใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-content>
-                {{ new Date(v.dsSignerCertificateDn.start) }}
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>วันหมดอายุใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-content>
-                {{ new Date(v.dsSignerCertificateDn.end) }}
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>ผลการตรวจสอบ</v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-content>
-                <v-list-item-icon>
-                  <v-icon color="success" v-text="v.dsTrusted ? 'mdi-check-bold': 'mdi-window-close'"></v-icon>
-                </v-list-item-icon>
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-group
-                v-if="v.tsSignerCertificateDn"
-                :value="true"
-                no-action
-                sub-group
-                prepend-icon="mdi-timer-check-outline"
-            >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title>Embedded Timestamp</v-list-item-title>
-                </v-list-item-content>
-              </template>
-
+            <div class="bg-light">
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>ผลการตรวจสอบการประทับรับรองเวลา</v-list-item-title>
+                  <v-list-item-title>ผลการตรวจสอบ</v-list-item-title>
                 </v-list-item-content>
 
                 <v-list-item-content>
-                  <v-list-item-title>{{ v.tsSignerCertificateDn.subjectDn.organization }}</v-list-item-title>
+                  <v-list-item-icon>
+                    <v-icon color="success" v-text="v.dsTrusted ? 'mdi-check-bold': 'mdi-window-close'"></v-icon>
+                  </v-list-item-icon>
                 </v-list-item-content>
               </v-list-item>
 
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>หน่วยงานผู้ประทับรับรองเวลา</v-list-item-title>
+                  <v-list-item-title>ผู้ลงลายมือชื่อดิจิตอล</v-list-item-title>
                 </v-list-item-content>
 
-                <v-list-item-content>
-                  <v-list-item-title>{{ v.tsSignerCertificateDn.subjectDn.commonName }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-list-item link>
-                <v-list-item-content>
-                  <v-list-item-title>ผู้ออกใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
-                </v-list-item-content>
-
-                <v-list-item-content>
-                  <v-list-item-title>{{ v.tsSignerCertificateDn.issuerDn.commonName }}</v-list-item-title>
+                <v-list-item-content :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                  {{ v.dsSignerCertificateDn.issuerDn.commonName }}
                 </v-list-item-content>
               </v-list-item>
 
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>วันออกใบรับรอง</v-list-item-title>
+                  <v-list-item-title>ผู้ออกใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
                 </v-list-item-content>
 
+                <v-list-item-content :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                  {{ v.dsSignerCertificateDn.issuerDn.commonName }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>{{ new Date(v.tsSignerCertificateDn.start) }}</v-list-item-title>
+                  <v-list-item-title>วันออกใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-content :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                  {{ new Date(v.dsSignerCertificateDn.start) }}
                 </v-list-item-content>
               </v-list-item>
 
@@ -130,8 +69,14 @@
                   <v-list-item-title>วันหมดอายุใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
                 </v-list-item-content>
 
+                <v-list-item-content :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                  {{ new Date(v.dsSignerCertificateDn.end) }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item style="margin-top: 10px">
                 <v-list-item-content>
-                  <v-list-item-title>{{ new Date(v.tsSignerCertificateDn.end) }}</v-list-item-title>
+                  <v-list-item-title class="text-h6">Embedded Timestamp</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
 
@@ -146,90 +91,145 @@
                   </v-list-item-icon>
                 </v-list-item-content>
               </v-list-item>
-            </v-list-group>
-          </v-list-group>
-        </v-list-group>
-      </v-list>
 
-      <br>
-      <v-list class="bg-light">
-        <v-list-group
-            :value="true"
-            prepend-icon="mdi-file-account"
-        >
-          <template v-slot:activator>
-            <v-list-item-title>ข้อมูลใบรับรองประทับเวลา (e-Timestamp)</v-list-item-title>
-          </template>
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>หน่วยงานผู้ประทับรับรองเวลา</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ v.tsSignerCertificateDn.subjectDn.organization }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>ผู้ประทับรับรองเวลา</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ v.tsSignerCertificateDn.subjectDn.commonName }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>ผู้ออกใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ v.tsSignerCertificateDn.issuerDn.commonName }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>วันออกใบรับรอง</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ new Date(v.tsSignerCertificateDn.start) }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>วันหมดอายุใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
+                </v-list-item-content>
+
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.dsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ new Date(v.tsSignerCertificateDn.end) }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </div>
+          </v-list-group>
 
           <v-list-group
-              v-for="(v, k) in transaction.signatures"
-              :key="k"
               v-if="!v.dsSignerCertificateDn"
-              :value="true"
               no-action
-              sub-group
-              prepend-icon="mdi-timer-check-outline"
+              :value="true"
+              prepend-icon="mdi-timer-lock-open-outline"
           >
             <template v-slot:activator>
               <v-list-item-content>
-                <v-list-item-title>Timestamp</v-list-item-title>
+                <v-list-item-title>Rev.{{ k + 1 }} Timestamp</v-list-item-title>
               </v-list-item-content>
             </template>
 
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>ชื่อองค์กรประทับรับรองเวลา</v-list-item-title>
-              </v-list-item-content>
+            <div class="bg-light">
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>ชื่อองค์กรประทับรับรองเวลา</v-list-item-title>
+                </v-list-item-content>
 
-              <v-list-item-content>
-                <v-list-item-title>{{ v.tsSignerCertificateDn.subjectDn.commonName }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.tsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ v.tsSignerCertificateDn.subjectDn.commonName }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>ชื่อผู้ใหบริการใบรับรอง</v-list-item-title>
-              </v-list-item-content>
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>ชื่อผู้ใหบริการใบรับรอง</v-list-item-title>
+                </v-list-item-content>
 
-              <v-list-item-content>
-                <v-list-item-title>{{ v.tsSignerCertificateDn.issuerDn.commonName }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.tsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ v.tsSignerCertificateDn.issuerDn.commonName }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>วันออกใบรับรอง</v-list-item-title>
-              </v-list-item-content>
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>วันออกใบรับรอง</v-list-item-title>
+                </v-list-item-content>
 
-              <v-list-item-content>
-                <v-list-item-title>{{ new Date(v.tsSignerCertificateDn.start) }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.tsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ new Date(v.tsSignerCertificateDn.start) }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>วันหมดอายุใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
-              </v-list-item-content>
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>วันหมดอายุใบรับรองอิเล็กทรอนิกส์</v-list-item-title>
+                </v-list-item-content>
 
-              <v-list-item-content>
-                <v-list-item-title>{{ new Date(v.tsSignerCertificateDn.end) }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title :class="[v.tsTrusted ? 'text-lime-700' : 'text-red-500']">
+                    {{ new Date(v.tsSignerCertificateDn.end) }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-            <v-list-item link>
-              <v-list-item-content>
-                <v-list-item-title>ผลการตรวจสอบ</v-list-item-title>
-              </v-list-item-content>
+              <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title>ผลการตรวจสอบ</v-list-item-title>
+                </v-list-item-content>
 
-              <v-list-item-content>
-                <v-list-item-icon>
-                  <v-icon color="success" v-text="v.tsTrusted ? 'mdi-check-bold': 'mdi-window-close'"></v-icon>
-                </v-list-item-icon>
-              </v-list-item-content>
-            </v-list-item>
+                <v-list-item-content>
+                  <v-list-item-icon>
+                    <v-icon color="success" v-text="v.tsTrusted ? 'mdi-check-bold': 'mdi-window-close'"></v-icon>
+                  </v-list-item-icon>
+                </v-list-item-content>
+              </v-list-item>
+            </div>
           </v-list-group>
-        </v-list-group>
-      </v-list>
+        </v-list>
+      </div>
 
       <br>
       <v-expansion-panels
@@ -238,7 +238,7 @@
           v-model="panel"
       >
         <v-expansion-panel>
-          <v-expansion-panel-header class="bg-light">ข้อมูล Blockchain</v-expansion-panel-header>
+          <v-expansion-panel-header>ข้อมูล Blockchain</v-expansion-panel-header>
           <v-expansion-panel-content>
             <v-row>
               <v-col sm="6">
