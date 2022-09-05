782bc234229a590e4bc48aa185b5dec8270d3704
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2022-09-05 17:58:20 +0700

    update base_url

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -189,8 +189,8 @@ module.exports = {
                 '^/captcha-api': ""
             }
         },
-        '/timestamp/': process.env.BASE_URL_BACKEND,
-        '/signature/': process.env.BASE_URL_BACKEND
+        '/timestamp/': 'https://stamp-stg.exkasan.com',
+        '/signature/': 'https://stamp-stg.exkasan.com'
     }
 
 }
